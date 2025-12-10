53cf11374391690af166c56966b4b7463df86fb1
= <sagarsoni32896@gmail.com>
2025-12-11 03:17:02 +0530

    almost done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { dummyPostsData, dummyUserData } from '../assets/assets';
 import Loading from '../components/Loading';
+import UserProfileInfo from '../components/UserProfileInfo';
+import PostCard from '../components/PostCard';
+import moment from 'moment';
 
 
 const Profile = () => {
@@ -30,9 +33,52 @@ const Profile = () => {
             {user.cover_photo && <img src={user.cover_photo} alt='' className='w-full h-full object-cover'/>}
           </div>
           {/* User Info */}
-
+          <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit}/>
+        </div>
+        {/* Tabs */}
+        <div className='mt-6'>
+          <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+            {
+            ['posts','media','likes'].map((tab)=>(
+              <button onClick={()=>setActiveTab(tab)} key={tab} className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${activeTab === tab ? "bg-indigo-600 text-white":
+                "text-gray-600 hover:text-gray-900"
+              }`}>
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              </button>
+            ))
+            }
+          </div>
+          {/* Posts */}
+          {
+            activeTab === 'posts' && (
+              <div className='mt-6 flex flex-col items-center gap-6'>
+                {posts.map((post)=><PostCard key={post._id} post={post} />)}
+              </div>
+            )
+          }
+        {/* Media */}
+        {activeTab === 'media' && (
+          <div className='flex flex-wrap mt-6 max-w-6xl'>
+          {
+          posts.filter((post)=>post.image_urls.length > 0).map((post)=>(
+            <>
+            {post.image_urls.map((image,index)=>(
+              <Link target='_blank' className='group relative' to={image} key={index}>
+              <img src={image} alt="" key={index} className='w-64 aspect-video object-cover'/>
+              <p className='absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl text-white opacity-0 group-hover:opacity-100 transition-duration-300'
+              >posted {moment(post.createdAt).fromNow()}</p>
+              </Link>
+            ))}
+            </>
+          ))
+        }
+        </div>
+        )}
+        
         </div>
       </div>
+      {/* Edit Profile Model*/}
+      {showEdit && <p>Show profile edit</p>}
     </div>
   ) : (<Loading height='60vh' />)
 }
